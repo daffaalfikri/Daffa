2b19fae4bb32f9aa9161ad306712321cc08b56a7
daffaalfikri <149288996+daffaalfikri@users.noreply.github.com>
2023-12-23 17:31:04 +0700

    Add files via upload

---
+++ uas.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import pandas as pd
+from mlxtend.frequent_patterns import association_rules, apriori
+from mlxtend.preprocessing import TransactionEncoder
+
+df = pd.read_csv("Groceries data.csv")
+
+st.title("Market Analyst")
+
+
+def get_data(itemDescription='', year=''):
+    data = df.copy()
+    filtered = data.loc[
+        (data["itemDescription"].str.contains(itemDescription)) &
+        (data["year"].astype(str).str.contains(year))
+    ]
+    return filtered if not filtered.empty else "No Result"
+
+
+def user_input_features():
+    Product = st.selectbox("Member_number", ['12V U1 L&G 6', '6V GC2 ULTRA GOLF 12', 'AGM 12V 7AH .187 FASTON', '12V 18AH 310CCA AGM 12/0',
+                           '12V 31DT DURACELL ULTRA AGM 30', 'AGM 12V 5AH .187 FASTON', 'DC 12V 35AH J TERM', '12V 17.5AH 310 CCA', '1.5V IND AA ALK BULK', '1.5V ALKAL'])
+    itemDescription = st.selectbox("itemDescription", ['tropical fruit', 'whole milk', 'pip fruit',
+                                   'other vegetables', 'whole milk', 'rolls/buns', 'other vegetables', 'pot plants', 'whole milk'])
+    year = st.select_slider("year", list(map(str, range(1, 42))))
+    return itemDescription, year, Product
+
+
+itemDescription, year, Product = user_input_features()
+
+data = get_data(itemDescription.lower(), year)
+
+
+def encode(x):
+    if x <= 0:
+        return 0
+    elif x >= 1:
+        return 1
+
+
+if not isinstance(data, str):
+    val_counts = df["itemDescription"].value_counts()
+    product_count_pivot = val_counts.pivot_table(
+        index='itemDescription', columns='Member_number', values='Count', aggfunc='sum').fillna(0)
+    product_count_pivot = product_count_pivot.applymap(encode)
+
+    frequent_itemsets_plus = apriori(product_count_pivot, min_support=0.03,
+                                     use_colnames=True).sort_values('support', ascending=False).reset_index(drop=True)
+
+    rules = association_rules(frequent_itemsets_plus, metric='lift',
+                              min_threshold=1).sort_values('lift', ascending=False).reset_index(drop=True)[["antecedents", "consequents", "support", "confidence", "lift"]]
+    rules.sort_values('confidence', ascending=False, inplace=True)
+
+
+def parse_list(x):
+    x = list(x)
+    if len(x) == 1:
+        return x[0]
+    elif len(x) > 1:
+        return ", ".join(x)
+
+
+def return_product_df(product_antecedents):
+    data = rules[["antecedents", "consequents"]].copy()
+
+    data["antecedents"] = data["antecedents"].apply(parse_list)
+    data["consequents"] = data["consequents"].apply(parse_list)
+
+    return list(data.loc[data["antecedents"] == product_antecedents].iloc[0, :])
+
+
+if type(data) != type("No Result"):
+    st.markdown("Rekomendasi: ")
+    st.success(
+        f"Jika konsumen membeli **{product}**, maka membeli **{return_product_df(product)[1]}** secara bersamaan")
